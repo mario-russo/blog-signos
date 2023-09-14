56ef44030490b05231e91c606eb0d1587c566567
Mário Russo <mario.russo93@hotmail.com>
2023-09-14 00:41:58 -0300

    refactor (id signo) : id para o signo

--- src/main/java/mario/russo/core/domain/SignoZodiaco.java
+++ src/main/java/mario/russo/core/domain/SignoZodiaco.java
@@ -1,25 +1,31 @@
 package mario.russo.core.domain;
 
 public enum SignoZodiaco {
-    ARIES("Áries", "21 de março - 19 de abril"),
-    TOURO("Touro", "20 de abril - 20 de maio"),
-    GEMEOS("Gêmeos", "21 de maio - 20 de junho"),
-    CANCER("Câncer", "21 de junho - 22 de julho"),
-    LEAO("Leão", "23 de julho - 22 de agosto"),
-    VIRGEM("Virgem", "23 de agosto - 22 de setembro"),
-    LIBRA("Libra", "23 de setembro - 22 de outubro"),
-    ESCORPIAO("Escorpião", "23 de outubro - 21 de novembro"),
-    SAGITÁRIO("Sagitário", "22 de novembro - 21 de dezembro"),
-    CAPRICÓRNIO("Capricórnio", "22 de dezembro - 19 de janeiro"),
-    AQUÁRIO("Aquário", "20 de janeiro - 18 de fevereiro"),
-    PEIXES("Peixes", "19 de fevereiro - 20 de março");
+    ARIES(1, "Áries", "21 de março - 19 de abril"),
+    TOURO(2, "Touro", "20 de abril - 20 de maio"),
+    GEMEOS(3, "Gêmeos", "21 de maio - 20 de junho"),
+    CANCER(4, "Câncer", "21 de junho - 22 de julho"),
+    LEAO(5, "Leão", "23 de julho - 22 de agosto"),
+    VIRGEM(6, "Virgem", "23 de agosto - 22 de setembro"),
+    LIBRA(7, "Libra", "23 de setembro - 22 de outubro"),
+    ESCORPIAO(8, "Escorpião", "23 de outubro - 21 de novembro"),
+    SAGITÁRIO(9, "Sagitário", "22 de novembro - 21 de dezembro"),
+    CAPRICÓRNIO(10, "Capricórnio", "22 de dezembro - 19 de janeiro"),
+    AQUÁRIO(11, "Aquário", "20 de janeiro - 18 de fevereiro"),
+    PEIXES(12, "Peixes", "19 de fevereiro - 20 de março");
 
     private final String nome;
     private final String datas;
+    private final int id;
 
-    SignoZodiaco(String nome, String datas) {
+    SignoZodiaco(int id, String nome, String datas) {
         this.nome = nome;
+        this.id = id;
         this.datas = datas;
+    }
+
+    public int getId() {
+        return this.id;
     }
 
     public String getNome() {
