e92e3f2c9cd7daacf0a6518c58f0f077a74fb3d4
Mario Russo <mario.russo93@hotmail.com>
2023-09-19 21:54:14 -0300

    Refactor (Panache Repository) trocando por Panache repository base

--- src/main/java/mario/russo/infra/ConteudoPanache.java
+++ src/main/java/mario/russo/infra/ConteudoPanache.java
@@ -2,12 +2,12 @@ package mario.russo.infra;
 
 import java.util.List;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import mario.russo.infra.entity.ConteudoEntity;
 
 @ApplicationScoped
-public class ConteudoPanache implements PanacheRepository<ConteudoEntity> {
+public class ConteudoPanache implements PanacheRepositoryBase<ConteudoEntity, Long> {
     public List<ConteudoEntity> findBySigno(String signo) {
         return list("signo", signo);
     }
--- src/main/java/mario/russo/infra/UsuarioPanache.java
+++ src/main/java/mario/russo/infra/UsuarioPanache.java
@@ -1,12 +1,12 @@
 package mario.russo.infra;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import mario.russo.infra.entity.UsuarioEntity;
 
 @ApplicationScoped
-public class UsuarioPanache implements PanacheRepository<UsuarioEntity>{
-    public UsuarioEntity findByEmail(String email){
+public class UsuarioPanache implements PanacheRepositoryBase<UsuarioEntity, Long> {
+    public UsuarioEntity findByEmail(String email) {
         return find("email", email).firstResult();
     }
 }
