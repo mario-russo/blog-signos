d4c1a16d69c9f1460a9ef3647a3f0eb73b24e145
Mário Russo <mario.russo93@hotmail.com>
2023-09-22 23:10:42 -0300

    feat( DTO ) : conteudo dto

--- src/main/java/mario/russo/core/dto/ConteudoResponseDTO.java
+++ src/main/java/mario/russo/core/dto/ConteudoResponseDTO.java
@@ -1,5 +1,18 @@
 package mario.russo.core.dto;
 
-public record ConteudoResponseDTO (){
-    
+import mario.russo.core.domain.SignoZodiaco;
+import mario.russo.core.domain.entity.ConteudoEntity;
+
+public record ConteudoResponseDTO(
+        Long id,
+        String conteudo,
+        String referencia,
+        SignoZodiaco signo,
+        UsuarioResponseDTO usuario) {
+    public ConteudoResponseDTO(ConteudoEntity entity) {
+        this(entity.getId(), entity.getConteudo(), entity.getReferencia(), entity.getSigno(),
+                new UsuarioResponseDTO(entity.usuario.getId(), entity.usuario.getNome(),
+                        entity.usuario.getEmail()));
+    }
+
 }
