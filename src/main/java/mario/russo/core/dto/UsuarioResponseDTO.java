099301cecf68e8a382729e63d9d9931094d34b66
Mario Russo <mario.russo93@hotmail.com>
2023-12-16 00:39:55 -0300

    Refactor : remoção de comentario

--- src/main/java/mario/russo/core/dto/UsuarioResponseDTO.java
+++ src/main/java/mario/russo/core/dto/UsuarioResponseDTO.java
@@ -9,18 +9,11 @@ public record UsuarioResponseDTO(
                 String nome,
                 String email,
                 List<Rules> rules) {
-        // public UsuarioResponseDTO(Long id, String nome, String email, Set<Rules>
-        // rules) {
-        // this.id = id;
-        // this.nome = nome;
-        // this.email = email;
-        // this.rules = rules;
-        // }
 
         public UsuarioResponseDTO(UsuarioEntity usuarioEntity) {
-               this( usuarioEntity.getId(),
-                 usuarioEntity.getNome(),
-                 usuarioEntity.getEmail(),
-                 usuarioEntity.getRules());
+                this(usuarioEntity.getId(),
+                                usuarioEntity.getNome(),
+                                usuarioEntity.getEmail(),
+                                usuarioEntity.getRules());
         }
 }
