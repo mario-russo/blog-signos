b500f23765aa1b6c826ff4d280ea6d9a7e5c64a5
Mario Russo <mario.russo93@hotmail.com>
2023-12-16 00:38:11 -0300

    Refactor : adição de user ao DTO

--- src/main/java/mario/russo/core/dto/AuthenticationTokenDTO.java
+++ src/main/java/mario/russo/core/dto/AuthenticationTokenDTO.java
@@ -1,10 +1,14 @@
 package mario.russo.core.dto;
 
 public class AuthenticationTokenDTO {
-    public String token;
+    private String token;
+    private Long id;
+    private String nome;
 
-    public AuthenticationTokenDTO(String token) {
-        this.token = token;
+    public AuthenticationTokenDTO(String token, Long id, String nome) {
+        setToken(token);
+        setId(id);
+        setNome(nome);
     }
 
     public String getToken() {
@@ -13,6 +17,22 @@ public class AuthenticationTokenDTO {
 
     public void setToken(String token) {
         this.token = token;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
 }
