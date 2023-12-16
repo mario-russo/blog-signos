610977dec70dd758205a3436a74ecea7532ee27c
Mario Russo <mario.russo93@hotmail.com>
2023-12-16 00:41:26 -0300

    Refactor : varchar com 50 letras  no nome

--- src/main/resources/db/migration/prod/V1.0__create_init.sql
+++ src/main/resources/db/migration/prod/V1.0__create_init.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS conteudo (
     id SERIAL PRIMARY KEY,
-    signo VARCHAR,
-    conteudo TEXT,
-    referencia VARCHAR,
+    signo VARCHAR(30),
+    conteudo VARCHAR(1500),
+    referencia VARCHAR(30),
     usuario_id INT,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 CREATE TABLE IF NOT EXISTS usuario (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR,
+    nome VARCHAR(50),
     email VARCHAR UNIQUE,
     senha VARCHAR,
     rules SMALLINT[],
