901aa671631084ef1f96753fc7891f6e980ed1a3
Mario Russo <mario.russo93@hotmail.com>
2023-12-19 01:13:20 -0300

    Fix : correção de erros com flyway e migração - Correção no script sql para iniciar com banco de dados ativo

--- src/main/resources/db/migration/dev/V1.0__create_init.sql
+++ src/main/resources/db/migration/dev/V1.0__create_init.sql
@@ -1,11 +1,3 @@
-CREATE TABLE conteudo (
-    id SERIAL PRIMARY KEY,
-    signo VARCHAR(30),
-    conteudo TYPE VARCHAR(1500);,
-    referencia VARCHAR(30),
-    usuario_id INT,
-    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
-);
 CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(50),
@@ -13,12 +5,25 @@ CREATE TABLE usuario (
     senha VARCHAR,
     rules SMALLINT[],
     -- Supondo que "conteudo" tem uma coluna chamada "usuario_id" como chave estrangeira
-    conteudo_id INT,
-    FOREIGN KEY (conteudo_id) REFERENCES conteudo(id)
+    conteudo_id INT
 );
+CREATE TABLE conteudo (
+    id SERIAL PRIMARY KEY,
+    signo VARCHAR(30),
+    conteudo VARCHAR(1500),
+    referencia VARCHAR(30),
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
+);
+
+ALTER TABLE usuario
+ADD CONSTRAINT fk_conteudo
+FOREIGN KEY (conteudo_id) REFERENCES conteudo(id);
+
+
 INSERT INTO usuario (nome, email, senha, rules)
 VALUES
-  ('mario ', 'mario@example.com', 'senha', ARRAY[0,1]),
+  ('mario russo', 'mario.russo93@hotmail.com', '1234', ARRAY[0,1]),
   ('russo', 'russo@example.com', 'senha', ARRAY[1.0]),
   ('araujo', 'araujo@example.com', 'senha', ARRAY[1]),
   ('goncalves', 'goncalves@example.com', 'senha', ARRAY[0]),
