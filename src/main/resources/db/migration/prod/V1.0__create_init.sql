9c9eea430b116c1c42aa8054500c95a76e99905a
Mário Russo <mario.russo93@hotmail.com>
2023-12-22 02:50:00 -0300

    Refactor : SQL script - Criação  de usuário padrão para aplicação

--- src/main/resources/db/migration/prod/V1.0__create_init.sql
+++ src/main/resources/db/migration/prod/V1.0__create_init.sql
@@ -1,4 +1,13 @@
-CREATE TABLE IF NOT EXISTS conteudo (
+CREATE TABLE usuario (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(50),
+    email VARCHAR UNIQUE,
+    senha VARCHAR,
+    rules SMALLINT[],
+    -- Supondo que "conteudo" tem uma coluna chamada "usuario_id" como chave estrangeira
+    conteudo_id INT
+);
+CREATE TABLE conteudo (
     id SERIAL PRIMARY KEY,
     signo VARCHAR(30),
     conteudo VARCHAR(1500),
@@ -6,13 +15,11 @@ CREATE TABLE IF NOT EXISTS conteudo (
     usuario_id INT,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
-CREATE TABLE IF NOT EXISTS usuario (
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(50),
-    email VARCHAR UNIQUE,
-    senha VARCHAR,
-    rules SMALLINT[],
-    -- Supondo que "conteudo" tem uma coluna chamada "usuario_id" como chave estrangeira
-    conteudo_id INT,
-    FOREIGN KEY (conteudo_id) REFERENCES conteudo(id)
-);
+
+ALTER TABLE usuario
+ADD CONSTRAINT fk_conteudo
+FOREIGN KEY (conteudo_id) REFERENCES conteudo(id);
+
+INSERT INTO usuario (nome, email, senha, rules)
+VALUES
+  ('mario russo', 'root@email.com', '1234', ARRAY[0,1]);
