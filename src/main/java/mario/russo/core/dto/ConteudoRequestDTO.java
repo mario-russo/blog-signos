df099a3636cfad0d536dffa6536a94e7464912bb
Mário Russo <mario.russo93@hotmail.com>
2023-12-31 20:12:21 -0300

    refactor: adicionando  tipo no dto

--- src/main/java/mario/russo/core/dto/ConteudoRequestDTO.java
+++ src/main/java/mario/russo/core/dto/ConteudoRequestDTO.java
@@ -1,12 +1,14 @@
 package mario.russo.core.dto;
 
 import mario.russo.core.domain.SignoZodiaco;
+import mario.russo.core.domain.TipoReferencia;
 
 public record ConteudoRequestDTO(
-        int id,
-        SignoZodiaco signo,
-        String conteudo,
-        String referencia,
-        int idUsuario) {
+                int id,
+                TipoReferencia tipo,
+                SignoZodiaco signo,
+                String conteudo,
+                int referencia,
+                int idUsuario) {
 
 }
