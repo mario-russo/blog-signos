39d475afb3ccc563a855a0e1874a6f15f2fba7bf
Mário Russo <mario.russo93@hotmail.com>
2023-12-31 20:13:14 -0300

    Refactor: atributo tipo

--- src/main/java/mario/russo/core/dto/ConteudoResponseDTO.java
+++ src/main/java/mario/russo/core/dto/ConteudoResponseDTO.java
@@ -1,18 +1,21 @@
 package mario.russo.core.dto;
 
 import mario.russo.core.domain.SignoZodiaco;
+import mario.russo.core.domain.TipoReferencia;
 import mario.russo.core.domain.entity.ConteudoEntity;
 import mario.russo.core.domain.entity.UsuarioEntity;
 
 public record ConteudoResponseDTO(
         int id,
         String conteudo,
-        String referencia,
+        int referencia,
         SignoZodiaco signo,
-        UsuarioResponseDTO usuario) {
+        UsuarioResponseDTO usuario,
+        TipoReferencia tipo) {
+
     public ConteudoResponseDTO(ConteudoEntity entity) {
         this(entity.getId(), entity.getConteudo(), entity.getReferencia(), entity.getSigno(),
-                usuarioFactory(entity.getUsuario()));
+                usuarioFactory(entity.getUsuario()), entity.getTipo());
     }
 
     private static UsuarioResponseDTO usuarioFactory(UsuarioEntity entity) {
