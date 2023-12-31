689939d6d7a7d001a1ad3c26751d01c22ffa8290
Mário Russo <mario.russo93@hotmail.com>
2023-12-31 20:10:21 -0300

    Refactor : adicionado o tipo de referencia

--- src/main/java/mario/russo/core/domain/entity/ConteudoEntity.java
+++ src/main/java/mario/russo/core/domain/entity/ConteudoEntity.java
@@ -9,6 +9,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import mario.russo.core.domain.SignoZodiaco;
+import mario.russo.core.domain.TipoReferencia;
 
 @Entity
 @Table(name = "conteudo")
@@ -16,21 +17,26 @@ public class ConteudoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private SignoZodiaco signo;
+
+    private TipoReferencia tipo;
 
     private String conteudo;
 
-    private String referencia;
+    private int referencia;
 
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "usuario_id")
     public UsuarioEntity usuario;
 
-    public ConteudoEntity(SignoZodiaco signo, String conteudo, String referencia, UsuarioEntity usuario) {
+    public ConteudoEntity(SignoZodiaco signo, TipoReferencia tipo, String conteudo, int referencia,
+            UsuarioEntity usuario) {
         this.signo = signo;
         this.conteudo = conteudo;
         this.referencia = referencia;
         this.usuario = usuario;
+        this.tipo = tipo;
     }
 
     public ConteudoEntity() {
@@ -60,11 +66,11 @@ public class ConteudoEntity {
         this.conteudo = conteudo;
     }
 
-    public String getReferencia() {
+    public int getReferencia() {
         return referencia;
     }
 
-    public void setReferencia(String referencia) {
+    public void setReferencia(int referencia) {
         this.referencia = referencia;
     }
 
@@ -74,6 +80,14 @@ public class ConteudoEntity {
 
     public void setUsuario(UsuarioEntity usuario) {
         this.usuario = usuario;
+    }
+
+    public TipoReferencia getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoReferencia tipo) {
+        this.tipo = tipo;
     }
 
     @Override
